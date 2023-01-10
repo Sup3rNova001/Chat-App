9edb383928e5360f1ae5febb35a0284ec35138c6
Sup3rNova001 <121170865+Sup3rNova001@users.noreply.github.com>
2023-01-10 06:22:00 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,66 @@
+const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
+const express = require("express");
+const formatMessage = require("./utils/messages");
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utils/user");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const BOT = "ChatApp Bot";
+
+io.sockets.on("connection", (socket) => {
+  console.log("New connection");
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+
+    socket.emit("message", formatMessage(BOT, "Welcome to chatApp"));
+
+    socket.broadcast
+      .to(user.room)
+      .emit("message", formatMessage(BOT, `${user.username} joined the chat`));
+
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+  });
+
+  socket.on("chatMessage", (msg) => {
+    const user = getCurrentUser(socket.id);
+    io.to(user.room).emit("message", formatMessage(user.username, msg));
+  });
+
+  socket.on("disconnect", () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        formatMessage(BOT, `${user.username} left the chat`)
+      );
+
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
+  });
+});
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
